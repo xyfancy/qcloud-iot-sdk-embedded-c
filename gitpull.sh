d11a1cddebe49a156428aeef3bc4c81893029f37
xyfancy <254615143@qq.com>
2019-06-15 11:12:05 +0800

    update

--- gitpull.sh
+++ gitpull.sh
@@ -1,4 +1,3 @@
+git pull
 git remote add upstream https://github.com/tencentyun/qcloud-iot-sdk-embedded-c.git
-git fetch upstream
-git checkout master
-git merge upstream/master
+git pull upstream master
